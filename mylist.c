9e0d78f67bf8d975232f49b0b0c3a92b25423be7
Ilya Dementev <ilya.dementev@gmail.com>
2020-04-03 11:09:43 -0400

    suggestion on count_list_items + main() for test

--- mylist.c
+++ mylist.c
@@ -1,3 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX_LIST_ITEMS_COUNT INT_MAX
+
 typedef struct list_s
 {
         struct list_s *next; /* NULL for the last item in a list */
@@ -7,6 +13,30 @@ list_t;
 
 	/* Counts the number of items in a list.
 	 */
+int count_list_items_straight(list_t *head) {
+
+    int count = 0;
+    list_t *p = head;
+    while  (NULL!= p){
+        if (count < MAX_LIST_ITEMS_COUNT){
+            count++;
+            p = p->next;
+        } else {
+            fprintf(stderr, "List oversize detected while counting items\n");
+            return (-1);
+        }            
+    }
+    return (count);    
+}
+
+int count_list_items_recurcive (const list_t *head) {
+    if (NULL != head){
+        return count_list_items_recurcive(head->next) + 1;
+	} else {
+		return 0;
+	}    
+}
+/*
 int count_list_items(const list_t *head) {
 	if (head->next) {
 		return count_list_items(head->next) + 1;
@@ -14,6 +44,11 @@ int count_list_items(const list_t *head) {
 		return 1;
 	}
 }
+*/
+/*
+1. no check for NULL list
+2. for large list overflow of stack can happen so non-recurcive variant can be appropriate
+*/
 
     /* Inserts a new list item after the one specified as the argument.
 	 */
@@ -88,3 +123,76 @@ char *item_data(const list_t *list)
 }
 */
 // 1. buf is in local scope of item_data function. buf shoud be passed as argument
+
+void free_list(list_t *list){
+    list_t *p = list;
+    list_t *garbage = NULL;
+
+    while (NULL != p){
+        if (NULL != p->next){
+            garbage = p;
+            p = p->next;
+            free(garbage);
+            //garbage = NULL;
+        } else{
+            free(p);
+            p = NULL;
+        }            
+    }
+}
+
+void print_all_list_items(list_t *list){
+    list_t *p = list;
+    int i = 1;
+    char str[12]; 
+
+    printf("Printing list values\n");
+    while (NULL != p){
+        item_data(p,str);
+        printf("value #%d == %s\n",i, str);
+        p = p->next;
+        i++;
+    }
+}
+
+void main () {
+    //printf("Hello World\n");
+    list_t *head = insert_next_to_list(NULL,10);
+    list_t *p = NULL;    
+    int i, count = 0;
+
+    count = count_list_items_recurcive(NULL);
+    printf("count r == %d\n",count);
+    count = count_list_items_straight(NULL);
+    printf("count s == %d\n",count);
+
+    count = count_list_items_recurcive(head);
+    printf("count r == %d\n",count);
+    count = count_list_items_straight(head);
+    printf("count s == %d\n",count);
+
+    insert_next_to_list(head,20);
+    count = count_list_items_recurcive(head);
+    printf("count r == %d\n",count);
+    count = count_list_items_straight(head);
+    printf("count s == %d\n",count);
+
+    insert_next_to_list(head,30);
+    count = count_list_items_recurcive(head);
+    printf("count r == %d\n",count);
+    count = count_list_items_straight(head);
+    printf("count s == %d\n",count);
+
+    print_all_list_items(head);
+    
+    remove_next_from_list(head);
+    print_all_list_items(head);
+
+    remove_next_from_list(head);
+    print_all_list_items(head);
+
+    remove_next_from_list(head);
+    print_all_list_items(head);
+
+    free_list(head);
+}
